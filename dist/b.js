28347ad69a5325c20b1cea5835c4b86b80c7ca6a
an244 <thienan.le244@gmail.com>
2017-10-15 22:09:43 +0700

    class, func & generic

--- dist/b.js
+++ dist/b.js
@@ -10,3 +10,50 @@ class Person {
         this.age = age;
     }
 }
+let a = 'abcd';
+//<string> là để thông báo với TS trong trg hợp này a là chuỗi
+//và 'a' sẽ có toàn bộ thuộc tính của chuỗi.
+console.log(a.length);
+let ab = { x: 10, y: 10 };
+//cach 1:
+function callSomething(obj) {
+    console.log('This is ' + obj.name);
+}
+callSomething({ name: 'Table' });
+//cach 2:
+function callSomething2(obj) {
+    console.log('This is ' + obj.name);
+}
+callSomething2({ name: 'Table' });
+function callSomething3(obj) {
+    console.log('This is ' + obj.name);
+}
+callSomething3({ name: 'Table' });
+//class nay fai co nme: string;
+class Animal {
+    constructor(nme, age) {
+        this.nme = nme;
+        this._age = age;
+    }
+    get ageA() {
+        return this._age;
+    }
+}
+function callSomething4(obj) {
+    console.log('This is ' + obj.nme);
+}
+const conBo = new Animal('Kobe1', 10);
+callSomething4(conBo);
+console.log(conBo.ageA);
+const AnT = { lastname: 'An' };
+//generic
+function getTotal(a, b) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const isNumber = typeof a === 'number' && typeof b === 'number';
+            if (!isNumber)
+                return reject(new Error('Type error'));
+            resolve(a + b);
+        });
+    });
+}
--- src/b.ts
+++ src/b.ts
@@ -19,3 +19,89 @@ class Person {
         this.age = age;
     }
 }
+
+let a: any = 'abcd';
+//<string> là để thông báo với TS trong trg hợp này a là chuỗi
+//và 'a' sẽ có toàn bộ thuộc tính của chuỗi.
+console.log((<string> a).length);
+
+
+interface Point {
+    x: number;
+    y: number;
+}
+
+let ab: Point = {x:10, y: 10};
+
+
+//cach 1:
+function callSomething(obj: any){
+    console.log('This is '+ obj.name)
+}
+
+callSomething({name: 'Table'});
+
+//cach 2:
+function callSomething2(obj: {name: string}){
+    console.log('This is '+ obj.name)
+}
+
+callSomething2({name: 'Table'});
+
+//cach 3:
+interface HasName{
+    name: string;
+}
+function callSomething3(obj: HasName){
+    console.log('This is '+ obj.name)
+}
+
+callSomething3({name: 'Table'});
+
+/////////
+
+interface HasNme {
+    nme: string;
+}
+
+//class nay fai co nme: string;
+class Animal implements HasNme {
+    nme: string;
+   private _age: number;
+    constructor(nme: string, age: number){
+        this.nme = nme;
+        this._age = age;
+    }
+
+    get ageA(){
+       return this._age; 
+    }
+}
+
+
+function callSomething4(obj: HasNme){
+    console.log('This is '+ obj.nme)
+}
+
+const conBo = new Animal('Kobe1',10);
+
+callSomething4(conBo);
+console.log(conBo.ageA);
+
+//////////
+interface HasName2{
+    lastname: string;
+    firstname?: string;//co hay ko co deu dc
+}
+const AnT: HasName2 = {lastname: 'An'};
+
+//generic
+function getTotal(a: number, b: number): Promise<number>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            const isNumber = typeof a === 'number'&& typeof b=== 'number';
+            if(!isNumber) return reject(new Error('Type error'));
+            resolve(a+b);
+        })
+    });
+}
